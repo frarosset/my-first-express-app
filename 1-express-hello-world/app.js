c6cbfe4d640bf305d1ddd29e8061ce0c66ef4e75
frarosset <37275581+frarosset@users.noreply.github.com>
2025-06-05 23:11:19 +0200

    Get the port the server is listening to from the environment variable PORT

--- 1-express-hello-world/app.js
+++ 1-express-hello-world/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+require("dotenv").config();
+
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => res.send("Hello world!"));
 
