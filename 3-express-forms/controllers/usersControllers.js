2139241c2b57d0481f9d39be7f601a8345d034b0
frarosset <37275581+frarosset@users.noreply.github.com>
2025-07-03 11:32:16 +0200

    feat: add first / last name input validation to Express forms lesson

--- 3-express-forms/controllers/usersControllers.js
+++ 3-express-forms/controllers/usersControllers.js
@@ -1,4 +1,24 @@
 const userStorage = require("../storages/usersStorage");
+const { body, validationResult } = require("express-validator");
+
+const maxLen = 10;
+const alphaErr = "must only contain letters";
+const lengthErr = `must be between 1 and ${maxLen} characters`;
+
+const validateUser = [
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: maxLen })
+    .withMessage(`First name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: maxLen })
+    .withMessage(`Last name ${lengthErr}`),
+];
 
 exports.usersListGet = (req, res) => {
   res.render("index", { title: "User list", users: userStorage.getUsers() });
@@ -8,8 +28,18 @@ exports.usersCreateGet = (req, res) => {
   res.render("createUser", { title: "Create user" });
 };
 
-exports.usersCreatePost = (req, res) => {
-  const { firstName, lastName } = req.body;
-  userStorage.addUser({ firstName, lastName });
-  res.redirect("/");
-};
+exports.usersCreatePost = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .render("createUser", { title: "Create user", errors: errors.array() });
+    }
+
+    const { firstName, lastName } = req.body;
+    userStorage.addUser({ firstName, lastName });
+    res.redirect("/");
+  },
+];
