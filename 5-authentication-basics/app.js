342d35eee61a50fa4aca5ab68e902b0924314bcf
frarosset <37275581+frarosset@users.noreply.github.com>
2025-10-04 19:04:00 +0200

    feat: save session secret in environment variable in authentication basics lesson

--- 5-authentication-basics/app.js
+++ 5-authentication-basics/app.js
@@ -15,7 +15,13 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.session());
 app.use((req, res, next) => {
   const msg = req.session.messages || [];
